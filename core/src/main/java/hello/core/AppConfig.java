519733f8c7b03e9e3663fd13c339831439754787
Cainhelpme <shindh0429@gmail.com>
2021-09-02 02:35:06 +0900

    Change Fixdiscount to Ratediscount

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,7 +2,10 @@ package hello.core;
 
 //App의 전체 동작 방식을 구성
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +14,23 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
         //생성자 통해서 객체 들어감. -생성자 주입
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+    //Refactoring 통해 각 class만의 역할 드러내기.
+    //Method 이름으로 역할을 Return type으로 구현클래스를 한눈에 알 수 있게 표현.
 }
