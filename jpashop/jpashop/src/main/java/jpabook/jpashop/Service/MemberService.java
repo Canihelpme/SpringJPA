8c8c6cc396554e76c45d4592f6805becf9d99eb7
DonghyunShin <shindh0429@gmail.com>
2022-01-21 23:03:22 +0900

    MemberRepo, MemberService

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+}
---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository //Component Scan에 의해 bean에 자동등록 됨.
+public class MemberRepository {
+
+    @PersistenceContext //Entity manager injection 위해.
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+    //단건 조회
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+        //==Member member = em.find(Member.class, id);
+    }
+
+    //여러개 조회
+   public List<Member> findAll() {
+        //query문(Entity를 대상으로) 과 return type 정의 후 return
+        //jpa는 객체에 대한 m
+        return em.createQuery("select m from Member m ", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+                                                        //Parameter binding
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+
+
+}
